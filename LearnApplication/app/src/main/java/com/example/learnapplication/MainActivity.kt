274d72e0f9fd1f72fe524f96933bd269325d62fa
Konstantinovskiy Arkady <arkady.konstant@mail.ru>
2024-07-31 13:57:44 +0300

    21 module

--- LearnApplication/app/src/main/java/com/example/learnapplication/MainActivity.kt
+++ LearnApplication/app/src/main/java/com/example/learnapplication/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.learnapplication
 
+import android.app.UiModeManager.MODE_NIGHT_AUTO
+import android.app.UiModeManager.MODE_NIGHT_NO
+import android.content.res.Resources.Theme
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import androidx.appcompat.app.AppCompatDelegate.NightMode
+import androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.learnapplication.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -17,8 +28,8 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setSupportActionBar(binding.appBarMain.toolbar)
+        initNavigation()
+//        setSupportActionBar(binding.appBarMain.topAppBar)
 
 
 
@@ -31,5 +42,56 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    private fun initNavigation() {
+        binding.appBarMain.topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    if (AppCompatDelegate.getDefaultNightMode() == MODE_NIGHT_YES){
+                        setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                        Toast.makeText(this, "Ночная тема выкл", Toast.LENGTH_SHORT).show()
+                    } else {
+                        setDefaultNightMode(MODE_NIGHT_YES)
+                        Toast.makeText(this, "Ночная тема вкл", Toast.LENGTH_SHORT).show()
+                    }
+
+                    true
+                }
+                else -> false
+            }
+        }
+
+        val navView: BottomNavigationView = binding.bottomNavigation
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_favorites, R.id.nav_watch_later, R.id.nav_selections
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+//        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+//
+//            when (it.itemId) {
+//                R.id.favorites -> {
+//                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+//                    true
+//                }
+//                R.id.watch_later -> {
+//                    Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
+//                    true
+//                }
+//                R.id.selections -> {
+//                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
     }
 }
